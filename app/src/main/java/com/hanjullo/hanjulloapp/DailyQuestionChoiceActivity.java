0e7540ca0837c3bb357fd2659b6559cc3777e663
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-10-29 23:38:51 +0900

    Fragment How Save

--- app/src/main/java/com/hanjullo/hanjulloapp/DailyQuestionChoiceActivity.java
+++ app/src/main/java/com/hanjullo/hanjulloapp/DailyQuestionChoiceActivity.java
@@ -1,14 +1,38 @@
 package com.hanjullo.hanjulloapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class DailyQuestionChoiceActivity extends AppCompatActivity {
 
+    private final int fragDailyHow = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_question);
+
+        FragmentSwitch(fragDailyHow);
+    }
+
+    private void FragmentSwitch(int fragCode) {
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (fragCode) {
+            case 1:
+                DailyHowFragment fragHow = new DailyHowFragment();
+
+                transaction.replace(R.id.fragFrame, fragHow);
+
+                transaction.commit();
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
     }
 }
