5125863a4006e9712b14c9311719d9dc0dfe97cb
Ichiya Luna <akkyu0724@gmail.com>
2021-11-20 02:07:51 +0900

    View Binding

--- app/src/main/java/com/hanjullo/hanjulloapp/ProfileActivity.java
+++ app/src/main/java/com/hanjullo/hanjulloapp/ProfileActivity.java
@@ -2,17 +2,24 @@ package com.hanjullo.hanjulloapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.hanjullo.hanjulloapp.databinding.ActivityProfileBinding;
+
 public class ProfileActivity extends AppCompatActivity {
 
     private boolean isTodayAnswerTrue = false;
+    private ActivityProfileBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        final View view = binding.getRoot();
+        setContentView(view);
 
         switchFragment(isTodayAnswerTrue);
     }
