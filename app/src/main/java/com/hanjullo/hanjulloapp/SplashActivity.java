96f3123551a01d5b209c891760eb63c7df0897ab
Ichiya Luna <akkyu0724@gmail.com>
2021-12-24 12:14:51 +0900

    loading and networking

--- app/src/main/java/com/hanjullo/hanjulloapp/SplashActivity.java
+++ app/src/main/java/com/hanjullo/hanjulloapp/SplashActivity.java
@@ -1,58 +1,83 @@
 package com.hanjullo.hanjulloapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.security.crypto.EncryptedSharedPreferences;
-import androidx.security.crypto.MasterKey;
 
-import android.content.SharedPreferences;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 public class SplashActivity extends AppCompatActivity {
-    private SharedPreferences userLoginPreferences;
+
+    private boolean loadingSkip;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        NetworkConnectionCheck network = NetworkConnectionCheck.getInstance(getApplicationContext());
+        network.register();
+
+        loadingSkip = false;
+
+        LoadingHandler handler = new LoadingHandler(Looper.getMainLooper());
+
+        LoadingTimerThread timerThread = new LoadingTimerThread(handler);
+
         try {
-            loadUserLogin();
+            InitialLoadThread loadThread = new InitialLoadThread(getApplicationContext(), handler);
+            loadThread.start();
         } catch (GeneralSecurityException e) {
             ExceptionToast.showExceptionToast(this,"20001", "자동 로그인 정보를 불러오지 못했습니다.");
+            loadingSkip = true;
         } catch (IOException e) {
             ExceptionToast.showExceptionToast(this,"10001", "자동 로그인 정보를 불러오지 못했습니다.");
+            loadingSkip = true;
         }
+
+        timerThread.start();
     }
 
-    private void checkNetwork() {
-        boolean isConnected;
-    }
+    class LoadingHandler extends Handler {
 
-    private void loadUserLogin() throws GeneralSecurityException, IOException {
-        boolean isAutoLogin;
-        String userID;
-        String userPW;
+        private boolean timerFinished;
+        private boolean loadingFinished;
 
-        MasterKey masterKey = new MasterKey.Builder(getApplicationContext(), MasterKey.DEFAULT_MASTER_KEY_ALIAS)
-                .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
-                .build();
+        public LoadingHandler(Looper looper) {
+            super(looper);
+            timerFinished = false;
+            loadingFinished = false;
+        }
 
-        userLoginPreferences = EncryptedSharedPreferences
-        .create(getApplicationContext(),
-                "LoginInfo",
-                masterKey,
-                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM);
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
 
-        isAutoLogin= userLoginPreferences.getBoolean("is_autologin",false);
+            if (!timerFinished) {
+                timerFinished = msg.getData().getBoolean("finished_timer");
+            }
 
-        if (isAutoLogin) {
-            userID = userLoginPreferences.getString("user_id", "");
-            userPW = userLoginPreferences.getString("user_pw", "");
-        } else {
+            if (!loadingFinished) {
+                loadingFinished = msg.getData().getBoolean("finished_loading");
+            }
 
+            Log.d("isFinished", "Handler: skipped - " + loadingSkip);
+            Log.d("isFinished", "Handler: timerFinished - " + timerFinished);
+            Log.d("isFinished", "Handler: loadingFinished - " + loadingFinished);
+
+            if (timerFinished && (loadingSkip || loadingFinished)) {
+                Intent intent = new Intent(SplashActivity.this, ProfileActivity.class);
+                startActivity(intent);
+                finish();
+            }
         }
     }
 }
